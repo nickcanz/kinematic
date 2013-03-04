b120245817e6f75664d6aa4e7043f7dbd01646eb
nickcanz <canzoneri.nicholas@gmail.com>
2013-03-03 22:41:36 -0500

    trying out clojure 1.5 dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.relaynetwork/kinematic "1.3.1-SNAPSHOT"
   :description          "Dynamic Web Development with Clojure"
-  :dev-dependencies     [[swank-clojure                        "1.4.2"]]
   :local-repo-classpath true
+  :plugins [[lein-swank "1.4.5"]]
   :lein-release     {:deploy-via :clojars :scm :git}
-  :dependencies     [[org.clojure/clojure                  "1.3.0"]
+  :dependencies     [[org.clojure/clojure                  "1.5.0"]
                      [org.clojure/data.json                "0.2.0"]
                      [org.clojure/tools.namespace          "0.1.0"]
                      [ring/ring-core                       "1.1.8"]
